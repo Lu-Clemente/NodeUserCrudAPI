b786338ea4a16be1d8c01d5bbedb72837ed49fbd
Luciene Clemente <luciene.clemente@unifei.edu.br>
2022-06-19 00:15:19 -0300

    MongoDB set up via mongoose

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 const port = 3000;
@@ -14,4 +16,17 @@ app.get('/', (req, res) => {
     res.json({ message: "Nothing here!" });
 });
 
-app.listen(port, () => console.log("Server running"));
+const DB_USER = process.env.DB_USER;
+const DB_PASS = encodeURIComponent(process.env.DB_PASSWORD);
+
+mongoose
+    .connect(
+        `mongodb+srv://${DB_USER}:${DB_PASS}@crudsimpleapicluster.gfzqe.mongodb.net/?retryWrites=true&w=majority`
+    )
+    .then(() => {
+        console.log("MongoDb connected!");
+        app.listen(port, () => console.log("Server running"));
+    })
+    .catch((err) => {
+        console.error(err);
+    });
